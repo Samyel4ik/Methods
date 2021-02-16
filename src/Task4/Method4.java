3bcfb743f615ae458c2b4aeae4eebc997f43ed29
Samyel4ik <samsoyk.evgen.92@mail.ru>
2021-02-16 14:34:54 +0300

    two

--- src/Task4/Method4.java
+++ src/Task4/Method4.java
@@ -28,21 +28,22 @@ public class Method4 {
         array[1] = num.nextInt();
         return array;
     }
-
-    public static double distanseMax(double array[][]) {
-
-        return Math.sqrt(Math.pow((array[0][0] - array[1][0]), 2) + Math.pow((array[0][1] - array[1][1]), 2));
+    
+    public static double distanceCalculation(double x1, double y1, double x2, double y2) {  //формула подсчета растояния между двумя точками.
+        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
     }
-        public static double distanse(double[][] array) {
-            double arrayN1[] = array[0];
-            double arrayN2[] = array[0];
-            double distanse = 0;
+
+    public static double distanse(double[][] array) {
+        double arrayN1[] = array[0];
+        double arrayN2[] = array[0];
+        double distanseMax = distanceCalculation(array[0][0], array[0][1], array[1][0], array[1][1]);
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                 distanse = Math.sqrt(Math.pow((array[i][0] - array[j][0]), 2) + Math.pow((array[i][1] - array[j][1]), 2));
-                if (distanseMax(array) < distanse) {
+            for (int j = 0; j < array.length; j++) {
+                double distanse = distanceCalculation(array[i][0], array[i][1], array[j][0], array[j][1]);
+                if (distanseMax < distanse) {
                     arrayN1 = array[i];
                     arrayN2 = array[j];
+                    distanseMax = distanse;
                 }
             }
         }
@@ -51,6 +52,6 @@ public class Method4 {
         System.out.println("Координата второй точки: ");
         System.out.println(Arrays.toString(arrayN2));
         System.out.println("Самое большое расстояни между данными двумя точками составило:");
-        return distanse;
+        return distanseMax;
     }
 }
